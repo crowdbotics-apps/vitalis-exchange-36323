9fba17a0b5c9005822c9ad0e638cdb5a2fa2075d
Crowdbotics <team@crowdbotics.com>
2022-07-20 23:52:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "vitalis_exchange_36323",
+  url: "https://vitalis_exchange_36323.botics.co",
+  api: "https://vitalis_exchange_36323.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
